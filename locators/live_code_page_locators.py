d215a4d8d2b416f19759deaf4eb2aea207afd545
QA/AQA Python Dzmitry Charukha <91555504+lambotik@users.noreply.github.com>
2023-10-08 11:07:26 +0300

    Update live_code_page_locators.py

--- locators/live_code_page_locators.py
+++ locators/live_code_page_locators.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.by import By
 
 
 class LiveCodePageLocators:
-    ELEMENTS = (By.XPATH, "//div[@class='card mt-4 top-card'][.='Elements']")
+    ELEMENTS = (By.XPATH, "//h5[.='Elements']")
     CHECKBOX = (By.XPATH, "//span[@class='text'][.='Check Box']")
     TOGGLE = (By.XPATH, "//button[@title='Toggle']")
     DOWNLOAD_TOGGLE = (By.CSS_SELECTOR, "li:nth-child(3) span button svg")
